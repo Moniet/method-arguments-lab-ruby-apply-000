02c0d133b66214ff72d8d84eae9ddd8ac6d2a824
moniet <moniet.sawhney@gmail.com>
2019-04-14 12:37:16 +0100

    added solution

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(x)
+  puts "Hi, my name is #{x}.\n"
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
